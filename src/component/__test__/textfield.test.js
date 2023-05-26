4e50a8e06967bd5f307248cf664555898f72e97d
mulyant20 <mulyan.t20@gmail.com>
2023-05-26 21:09:57 +0700

    update textfield unit test

--- src/component/__test__/textfield.test.js
+++ src/component/__test__/textfield.test.js
@@ -1,25 +1,24 @@
 import { fireEvent, render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
-import userEvent from '@testing-library/user-event'
 import Textfield from '../Textfield'
-import { useState } from 'react'
-
-const onSubmit = jest.fn()
 
 describe('test textfield component', () => {
-  test('textfield must have label', () => {
+  test('textfield should have label', () => {
     render(<Textfield label='username' />)
     expect(screen.getByText('username')).toBeInTheDocument()
   })
 
-  test('textfield must initialize with empty', () => {
+  test('textfield should initialize with empty', () => {
     render(<Textfield label='username' value='' placeholder='username here' />)
     const textfield = screen.getByPlaceholderText( /username here/i )
     expect(textfield.value).toBe('')
   })
 
-  test('textfield must have have username after typed', () => {
-    const [name, setUser] = useState('')
-    render()
+  test('textfield should have new value', () => {
+    render(<Textfield placeholder='username'/>)
+    const textfield = screen.getByPlaceholderText(/username/i)
+
+    fireEvent.change(textfield, { target: { value: 'zenitsu' } })
+    expect(textfield.value).toBe('zenitsu')
   })
 })
